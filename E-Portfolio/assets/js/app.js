5d3d33c8300966c805aa5d72a96cb2e8fece460c
Luke Hopkins <147019639+LukeHopkinss@users.noreply.github.com>
2024-04-07 01:14:20 -0400

    Add files via upload

---
+++ E-Portfolio/assets/js/app.js
@@ -0,0 +1,27 @@
+// Nav HamburgerBurger Selections
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav")
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+}
+);
+
+// Close menu when link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) => {
+    link.addEventListener("click", () => {
+        ul.classList.remove("show");
+    }
+    )
+}
+);
+
+
+
+
